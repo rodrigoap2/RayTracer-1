eaf19b4b632bb69db30f78a31bb7f81800618567
Daniel Bastos <dmrb2@cin.ufpe.br>
2019-05-22 09:36:13 -0300

    finalizacao step 5 e correcao de bugs

--- code/lib/hitable_list.h
+++ code/lib/hitable_list.h
@@ -6,7 +6,7 @@
 class hitable_list: public hitable {
     public:
         hitable_list() {}
-        hitable_list(hitable ** l, int n) {list = l; list_size = n}
+        hitable_list(hitable ** l, int n) {list = l; list_size = n;}
         virtual bool hit(const ray &r, float tmin, float tmax, hit_record & rec) const;
         hitable **list;
         int list_size;
--- code/step5_2.cpp
+++ code/step5_2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <bits/stdc++.h>
 #include "lib/sphere.h"
 #include "lib/hitable_list.h"
 #include "float.h"
@@ -21,13 +22,19 @@ int main() {
     vec3 horizontal(4, 0, 0);
     vec3 vertical(0, 2, 0);
     vec3 origin(0, 0, 0);
+    hitable *list[2];
+    list[0] = new sphere(vec3(0, 0, -1), 0.5);
+    list[1] = new sphere(vec3(0.0,-100.5,-1.0), 100);
+    hitable *world = new hitable_list(list,2);
     for(int i = ny-1; i > -1; i--) {
         for(int j = 0; j < nx; j++) {
             float u, v;
             u = float(j)/float(nx);
             v = float(i)/float(ny);
             ray r(origin, lower_left_corner + u*horizontal + v*vertical);
-            vec3 col = color(r);
+
+            vec3 p = r.point_at_parameter(2.0);
+            vec3 col = color(r, world);
             int ir = int(255.99*col[0]), ig = int(255.99*col[1]), ib = int(255.99*col[2]);
             printf("%i %i %i\n", ir, ig, ib);
         }
