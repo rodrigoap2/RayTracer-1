f9412de3fc4e52172bdd608141e8040402702936
Daniel Bastos <dmrb2@cin.ufpe.br>
2019-05-23 13:15:12 -0300

    ajuste main step8

--- code/step8.cpp
+++ code/step8.cpp
@@ -54,37 +54,3 @@ int main() {
         }
     }
 }
-
-// int main() {
-//     int nx = 200;
-//     int ny = 100;
-//     int ns = 10;
-//     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
-//     hitable *list[5];
-//     float R = cos(M_PI/4);
-//     list[0] = new sphere(vec3(0,0,-1), 0.5, new lambertian(vec3(0.1, 0.2, 0.5)));
-//     list[1] = new sphere(vec3(0,-100.5,-1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
-//     list[2] = new sphere(vec3(1,0,-1), 0.5, new metal(vec3(0.8, 0.6, 0.2)));
-//     hitable *world = new hitable_list(list,3);
-
-//     camera cam;
-
-//     for (int j = ny-1; j >= 0; j--) {
-//         for (int i = 0; i < nx; i++) {
-//             vec3 col(0, 0, 0);
-//             for (int s=0; s < ns; s++) {
-//                 float u = float(i + drand48()) / float(nx);
-//                 float v = float(j + drand48()) / float(ny);
-//                 ray r = cam.get_ray(u, v);
-//                 vec3 p = r.point_at_parameter(2.0);
-//                 col += color(r, world,0);
-//             }
-//             col /= float(ns);
-//             col = vec3( sqrt(col[0]), sqrt(col[1]), sqrt(col[2]) );
-//             int ir = int(255.99*col[0]); 
-//             int ig = int(255.99*col[1]); 
-//             int ib = int(255.99*col[2]); 
-//             std::cout << ir << " " << ig << " " << ib << "\n";
-//         }
-//     }
-// }
