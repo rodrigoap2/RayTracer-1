5e701f3daf350873a89c923659656bea7c32fe0c
Daniel Bastos <dmrb2@cin.ufpe.br>
2019-05-23 23:24:52 -0300

    fixed phong

--- code/lib/material.h
+++ code/lib/material.h
@@ -36,28 +36,28 @@ inline float max(float a, float b) {
 }
 
 vec3 reflect(const vec3 &v, const vec3 &n) {
-    return v- 2*dot(v,n)*n;
+    return v - 2*dot(v,n)*n;
 }
 
-vec3 phong(materialLight& light, const hit_record& rec, const camera& view) {
+vec3 phong(materialLight light, const hit_record& rec, const camera& view) {
     vec3 esf = rec.mat_ptr->color;
     //cerr << esf.e[0] << " " << esf.e[1] << " " << esf.e[2] << "\n";
-    vec3 lightDirection = normalize(light.position - rec.p);
-    vec3 viewDirection = normalize(view.origin - rec.p);
-    vec3 normaNormal = normalize(rec.normal);
+    vec3 lightDirection = normalize(light.position - rec.p); //l
+    vec3 viewDirection = normalize(view.origin - rec.p); //v
+    vec3 normaNormal = normalize(rec.normal); //n
 
-    float cosTheta = max(0.0, dot(normaNormal, lightDirection));
+    float cosTheta = max(0.0f, dot(normaNormal, lightDirection));
 
     //cores
 
-    vec3 ambient = (rec.mat_ptr->ka*light.color)*rec.mat_ptr->color;
+    vec3 ambient = rec.mat_ptr->ka*rec.mat_ptr->color*light.color;
     vec3 diffuse = vec3(0.0, 0.0, 0.0);
     vec3 specular = vec3(0.0, 0.0, 0.0);
+    vec3 relfectionDir = reflect(lightDirection, normaNormal);
 
     if(cosTheta > 0.0) {
-        vec3 relfectionDir = reflect(-lightDirection, normaNormal);
-        diffuse = rec.mat_ptr->kd * light.color * cosTheta * rec.mat_ptr->color;
-        specular = rec.mat_ptr->ks * light.color * pow(max(0.0, dot(relfectionDir, normaNormal)), 128.0) * rec.mat_ptr->color;
+        diffuse = rec.mat_ptr->kd * rec.mat_ptr->color * light.color * cosTheta;
+        specular = rec.mat_ptr->ks * rec.mat_ptr->color * light.color * pow(max(0.0, dot(viewDirection, relfectionDir)), 128.0);
     }
 
     return ambient + diffuse + specular;
--- code/step8_phong.cpp
+++ code/step8_phong.cpp
@@ -8,36 +8,38 @@
 
 using namespace std;
 
-vec3 color(const ray& r, hitable *world, int depth, materialLight& light, camera& view) {
+vec3 color(const ray& r, const hitable *world, const materialLight& light, const camera& view) {
     hit_record rec;
-    if(world->hit(r, 0.00000001, FLT_MAX, rec)) {
+    if(world->hit(r, 0.001, FLT_MAX, rec)) {
         hit_record hitted;
         if(world->hit(ray(rec.p, light.position-rec.p), 0.001, FLT_MAX, hitted)) {
             vec3 unit_direction = unit_vector(r.direction());
             float t = 0.5*(unit_direction.y()+1.0);
             return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
+            return vec3(0.0, 0.0, 0.0);
         }
         return phong(light, rec, view);
     }
     vec3 unit_direction = unit_vector(r.direction());
     float t = 0.5*(unit_direction.y()+1.0);
     return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
-    //return vec3(0.0, 0.0, 0.0);
+    return vec3(0.0, 0.0, 0.0);
 }
 
 int main() {
     int nx = 400;
     int ny = 200;
-    int ns = 100;
+    int ns = 1;
     printf("P3\n%i %i\n255\n", nx, ny);
-    hitable *list[4];
+    hitable *list[5];
     list[0] = new sphere(vec3(0, 0, -1), 0.5, new material(vec3(0.8, 0.3, 0.3), 0.2, 0.5, 0.6, 1.0));
     list[1] = new sphere(vec3(0.0,-100.5,-1.0), 100, new material(vec3(0.8, 0.8, 0.0), 0.2, 0.5, 0.6, 1.0));
     list[2] = new sphere(vec3(1,0,-1), 0.5, new material(vec3(0.8, 0.6, 0.2), 0.2, 0.5, 0.9, 1.0));
     list[3] = new sphere(vec3(-1,0,-1), 0.5, new material(vec3(0.8, 0.8, 0.8), 0.2, 0.5, 0.6, 1.0));
-    hitable *world = new hitable_list(list,4);
+    list[4] = new sphere(vec3(0,0,0), 5000.0, new material(vec3(0.8, 0.8, 0.8), 0.2, 0.5, 0.6, 1.0));
+    hitable *world = new hitable_list(list,5);
     camera cam;
-    materialLight light(vec3(1.0, 1.0, 1.0), vec3(1.0, 1000.0, -1.0));
+    materialLight light(vec3(1.0, 1.0, 1.0), vec3(1.0, 60.0, -1.0));
     for(int i = ny-1; i > -1; i--) {
         for(int j = 0; j < nx; j++) {
             vec3 col(0,0,0);
@@ -45,8 +47,8 @@ int main() {
                 float u = float (j+ drand48()) / float(nx);
                 float v = float (i+ drand48()) / float(ny);
                 ray r = cam.get_ray(u, v);
-                vec3 p = r.point_at_parameter(2.0);
-                col += color(r, world, 0, light, cam);
+                //vec3 p = r.point_at_parameter(2.0);
+                col += color(r, world, light, cam);
             }
             col /= float(ns);
             col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
