97efa763f92fcd8db9a09b793f81b2e0f7e3cca8
Daniel Bastos <dmrb2@cin.ufpe.br>
2019-05-24 12:17:28 -0300

    criacao step11 pt2

--- code/lib/material.h
+++ code/lib/material.h
@@ -15,6 +15,7 @@ class material {
         material(vec3 c, float a, float d, float s, float al) : color(c), ka(a), ks(s), kd(d), alpha(al) {}
 
         vec3 color;
+        float ke;
         float ka;
         float ks;
         float kd;
@@ -42,14 +43,13 @@ vec3 reflect(const vec3 &v, const vec3 &n) {
 vec3 phong(materialLight light, const hit_record& rec, const camera& view) {
     vec3 esf = rec.mat_ptr->color;
     //cerr << esf.e[0] << " " << esf.e[1] << " " << esf.e[2] << "\n";
-    vec3 lightDirection = normalize(light.position - rec.p); //l
-    vec3 viewDirection = normalize(view.origin - rec.p); //v
-    vec3 normaNormal = normalize(rec.normal); //n
+    vec3 lightDirection = normalize(light.position - rec.p);
+    vec3 viewDirection = normalize(view.origin - rec.p);
+    vec3 normaNormal = normalize(rec.normal);
 
     float cosTheta = max(0.0f, dot(normaNormal, lightDirection));
 
     //cores
-
     vec3 ambient = rec.mat_ptr->ka*rec.mat_ptr->color*light.color;
     vec3 diffuse = vec3(0.0, 0.0, 0.0);
     vec3 specular = vec3(0.0, 0.0, 0.0);
